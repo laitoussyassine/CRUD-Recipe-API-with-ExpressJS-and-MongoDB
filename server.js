5071e1d2490f4f9234477e2020922d0d8aa205a4
laitouss-yassine <yassinelaitouss2380@gmail.com>
2024-01-16 12:21:00 +0100

    add .env file

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+require('dotenv').config()
 const express = require("express");
 const Recipe = require('./models/recipe');
 const mongoose = require('mongoose');
 const app = express();
 const routes = require("./routes.js")
 
-mongoose.connect('mongodb://localhost:27017').then(() => {
+
+
+
+mongoose.connect(process.env.CONNECTION_STRING).then(() => {
     console.log('connected');
 }).catch((err) => {
     console.log(err);
@@ -16,5 +20,5 @@ app.use(express.json());
 app.use('/recipes', routes);
 
 
-const port = 8000;
+const port = process.env.PORT || 3000;
 app.listen(port, console.log(`listening on port ${port}`));
